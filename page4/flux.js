2f7842da8762b184a308f6cdf705ac5e120b6e51
Alexander Stone <alexstone977@gmail.com>
2025-05-03 22:13:32 -0400

    added pig big examples with flux

---
+++ page4/flux.js
@@ -0,0 +1,30 @@
+const fluxdata = [{
+    "uuid": '001',
+    "prompt": "pig vs big",
+    "contrast": "maximal",
+    "location": "initial",
+    "sound": "p",
+    "pair": {
+        "item1": {
+            "word": "pig",
+            "visual_prompt": "visual_prompt1",
+            "sound": "p",
+            "ipa": "pɪg",
+            "place": "bilabial",
+            "manner": "plosive",
+            "voicing": "voiceless",
+            "b64_image": "./flux/pig.jpg"
+        },
+        "item2": {
+            "word": "big",
+            "visual_prompt": "visual_prompt2",
+            "sound": "b",
+            "ipa": "bɪg",
+            "place": "bilabial",
+            "manner": "plosive",
+            "voicing": "voiced",
+            "b64_image": "./flux/big.jpg"
+        }
+    },
+    "explanation": "The words 'pig' and 'big' differ only in their initial sound. 'Pig' starts with a voiceless bilabial plosive /p/, while 'big' starts with a voiced bilabial plosive /b/. Both sounds are produced at the same place of articulation (bilabial) and with the same manner (plosive), but differ in voicing."
+}]; 
